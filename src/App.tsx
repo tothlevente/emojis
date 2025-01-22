081fb0ab36ae31d7f863ca7c250b4f475932c3fc
Levente <168279599+tothlevente@users.noreply.github.com>
2025-01-22 10:45:43 +0000

    Develop footer components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "./components/theme-provider";
 
 import EmojiList from "./components/contents/EmojiList";
 import Header from "./components/contents/Header";
+import Footer from "./components/contents/Footer";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
     >
       <Header />
       <EmojiList />
+      <Footer />
     </ThemeProvider>
   );
 }
