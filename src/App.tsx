a80f87cdb693f4f67b30c786a8af9598958c3075
Levente <168279599+tothlevente@users.noreply.github.com>
2025-02-05 11:04:33 +0000

    Changing folder names for better transparency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { ThemeProvider } from "./components/theme-provider";
 
-import EmojiList from "./components/contents/EmojiList";
-import Header from "./components/contents/Header";
-import Footer from "./components/contents/Footer";
+import EmojiList from "./components/layouts/EmojiList";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
 import Statement from "./components/statement";
 
 export default function App() {
